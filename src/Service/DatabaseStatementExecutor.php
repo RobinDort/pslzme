fd45ec0f22e89573bbbcb652ece93463edf60e1a
RobinDort <robin@alexanderdort.com>
2025-02-04 09:10:58 +0100

    Added another service called StatementPreparer

--- src/Service/DatabaseStatementExecutor.php
+++ src/Service/DatabaseStatementExecutor.php
@@ -1,7 +1,15 @@
 <?php
 namespace RobinDort\PslzmeLinks\Service;
 
-class DatabaseStatementExecutor {
+use RobinDort\PslzmeLinks\Service\StatementPreparer;
 
+class DatabaseStatementExecutor {
+    private $dbConn;
+    private $statementPreparer;
+
+    public function __construct(DatabaseConnection $db) {
+        $this->dbConn = $db;
+        $this->statementPreparer = new StatementPreparer($this->dbConn->getConnection());
+    }
 }
 ?>
---
+++ src/Service/StatementPreparer.php
@@ -0,0 +1,4 @@
+<?php
+namespace RobinDort\PslzmeLinks\Service;
+
+?>
