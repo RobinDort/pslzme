44b32ea7e49a7c0e237b2bc21ce6ebd4d228f87e
RobinDort <robin@alexanderdort.com>
2025-02-04 14:56:14 +0100

    Added the contao template folder as well as 2 custom rocksolid files that build a custom element that is needed to decrypt the pslzme queries

---
+++ src/Resources/contao/templates/rsce_query_decryption_config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'label' => array('Query decryption', ""),
+    'types' => array('module'),
+    'contentCategory' => 'media',
+    'moduleCategory' => 'miscellaneous',
+    'beTemplate' => 'be_wildcard',
+    'standardFields' => array(''),
+    'wrapper' => array(
+        'type' => 'none',
+    ),
+);
