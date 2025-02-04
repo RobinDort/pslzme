cfb8288a399b58977de62592270ee9e7c4c5434c
RobinDort <robin@alexanderdort.com>
2025-02-04 15:15:56 +0100

    Added a new frontend module that uses the custom element as template. Registered the module inside the also added config.php file

---
+++ src/Module/QueryDecryption.php
@@ -0,0 +1,16 @@
+<?php
+namespace RobinDort\PslzmeLinks\Module;
+
+use Contao\Module;
+use Contao\FrontendTemplate;
+
+class QueryDecryption extends Module {
+    protected $strTemplate = 'rsce_query_decryption';
+
+
+    /**
+     * Generate the module content
+     */
+    protected function compile(){}
+}
+?>
---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,5 @@
+<?php
+use RobinDort\PslzmeLinks\Module\QueryDecryption;
+
+$GLOBALS['FE_MOD']['Query-Decryption'] = QueryDecryption::class;
+?>
