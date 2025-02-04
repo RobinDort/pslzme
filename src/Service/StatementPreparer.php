a7178312a9ce9bbc34f2c2a0b0d88393298ca1dc
RobinDort <robin@alexanderdort.com>
2025-02-04 09:25:05 +0100

    Added the StatementPreparer class with its constructor as well as the first two functions that prepare statements used by the DatabaseStatementExecutor class

--- src/Service/StatementPreparer.php
+++ src/Service/StatementPreparer.php
@@ -1,4 +1,27 @@
 <?php
 namespace RobinDort\PslzmeLinks\Service;
 
+class StatementPreparer {
+    private $conn;
+
+    public function __construct($connection) {
+        $this->conn = $connection;
+    }
+
+    public function prepareSelectAllCustomer() {
+        $sqlQuery = "SELECT * FROM pslzme_kunde"; 
+        $stmt = $this->conn->prepare($sqlQuery);
+
+        return $stmt;
+    }
+
+    public function prepareSelectCustomerKey($queryParamID) {
+        $sqlQuery = "SELECT a.*, b.* FROM pslzme_kunde as a INNER JOIN encrypt_info AS b ON a.KundenID=b.PslzmeKundenID WHERE a.KundenID=?"; 
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->bind_param("i", $queryParamID);
+
+        return $stmt;
+    }
+}
+
 ?>
