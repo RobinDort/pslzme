72c96b62b07f0371d2cbdb3920b553993444b5f5
RobinDort <robin@alexanderdort.com>
2025-02-07 10:55:33 +0100

    Added the customTpl option as well

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -3,7 +3,7 @@
 use RobinDort\PslzmeLinks\Controller\FrontendModule\QueryDecryptionController;
 use Contao\Controller;
 
-$GLOBALS['TL_DCA']['tl_module']['palettes']['query_decryption'] = '{title_legend},name,type;{config_legend},{template_legend}';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['query_decryption'] = '{title_legend},name,type;{config_legend},{template_legend},customTpl;{protected_legend:hide},protected;{expert_legend:hide}';
 
 
 ?>
