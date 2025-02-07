349c49bbc43592271b0cd5439dd6355e46a0e26e
RobinDort <robin@alexanderdort.com>
2025-02-07 09:33:54 +0100

    Added a module class to handle the new frontend module

---
+++ src/Module/QueryDecryption.php
@@ -0,0 +1,12 @@
+<?php
+namespace RobinDort\PslzmeLinks\Module;
+
+use Contao\Module;
+
+class QueryDecryption extends Module {
+    protected $strTemplate = "mod_query_decryption";
+
+    protected function compile() {}
+}
+
+?>
