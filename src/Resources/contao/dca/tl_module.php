637dcb8fa20c42334fd2c779b0d383371e62af36
RobinDort <robin@alexanderdort.com>
2025-02-07 10:49:26 +0100

    change the custom template field to {template_legend} of contaos pallete option

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -3,16 +3,7 @@
 use RobinDort\PslzmeLinks\Controller\FrontendModule\QueryDecryptionController;
 use Contao\Controller;
 
-$GLOBALS['TL_DCA']['tl_module']['palettes']['query_decryption'] = '{title_legend},name,type;{config_legend},template;';
-
-// Define the 'template' field
-$GLOBALS['TL_DCA']['tl_module']['fields']['template'] = [
-    'label'     => ['Template', 'Select the template for the frontend module.'],
-    'inputType' => 'select',
-    'options'   => Controller::getTemplateGroup('mod_'), // Automatically loads templates prefixed with 'mod_'
-    'eval'      => ['mandatory' => true, 'chosen' => true, 'tl_class' => 'w50'],
-    'sql'       => "varchar(255) NOT NULL default ''"
-];
+$GLOBALS['TL_DCA']['tl_module']['palettes']['query_decryption'] = '{title_legend},name,type;{config_legend},{template_legend}';
 
 
 ?>
