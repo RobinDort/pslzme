2c76da926cf1667b2d104888b11d25f6b07e4298
RobinDort <robin@alexanderdort.com>
2025-02-07 09:51:51 +0100

    fixed wrong function call of wrong class

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,6 +1,7 @@
 <?php
 
 use RobinDort\PslzmeLinks\Controller\FrontendModule\QueryDecryptionController;
+use Contao\Controller;
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['query_decryption'] = '{title_legend},name,type;{config_legend},template;';
 
@@ -8,7 +9,7 @@ $GLOBALS['TL_DCA']['tl_module']['palettes']['query_decryption'] = '{title_legend
 $GLOBALS['TL_DCA']['tl_module']['fields']['template'] = [
     'label'     => ['Template', 'Select the template for the frontend module.'],
     'inputType' => 'select',
-    'options'   => \Contao\System::getTemplateGroup('mod_'), // Automatically loads templates prefixed with 'mod_'
+    'options'   => Controller::getTemplateGroup('mod_'), // Automatically loads templates prefixed with 'mod_'
     'eval'      => ['mandatory' => true, 'chosen' => true, 'tl_class' => 'w50'],
     'sql'       => "varchar(255) NOT NULL default ''"
 ];
