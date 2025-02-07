fbeb0a4cf09df1f0adc728824bb9f6bb98f8e4a4
RobinDort <robin@alexanderdort.com>
2025-02-07 12:07:47 +0100

    Renamed the class for that initializes the service yaml file. It must be the same as the bundle class name

--- src/DependencyInjection/PslzmeLinksExtension.php
+++ src/DependencyInjection/PslzmeLinksExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class PslzmeLinksExtension extends Extension {
+class RobinDortPslzmeLinksExtension extends Extension {
 
     public function load(array $configs, ContainerBuilder $container): void
     {
