1e0c2c3c5d2248e13a69fffffd11d998af76698d
RobinDort <robin@alexanderdort.com>
2025-02-10 14:59:21 +0100

    Due to not finding the functions, the last step is undone and the js file are located inside the head again

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,12 +2,12 @@
 use RobinDort\PslzmeLinks\Module\QueryDecryption;
 
 // Init all css / js files
-$GLOBALS['TL_BODY'][] = "bundles/robindortpslzmelinks/js/url-query-data-filter.js|static";
-$GLOBALS['TL_BODY'][] = "bundles/robindortpslzmelinks/js/cookie-extractor.js|static";
-$GLOBALS['TL_BODY'][] = "bundles/robindortpslzmelinks/js/api-request.js|static";
-$GLOBALS['TL_BODY'][] = "bundles/robindortpslzmelinks/js/redirect-cookie-acception.js|static";
-$GLOBALS['TL_BODY'][] = "bundles/robindortpslzmelinks/js/query-click-listener.js|static";
-$GLOBALS['TL_BODY'][] = "bundles/robindortpslzmelinks/js/main.js|static";
+$GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/url-query-data-filter.js|static";
+$GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/cookie-extractor.js|static";
+$GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/api-request.js|static";
+$GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/redirect-cookie-acception.js|static";
+$GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/query-click-listener.js|static";
+$GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/main.js|static";
 
 $GLOBALS['FE_MOD']['pslzme']['query_decryption'] = \RobinDort\PslzmeLinks\Module\QueryDecryption::class;
 
