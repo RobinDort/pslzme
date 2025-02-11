06b1f069a4e33012e15cd14fb5a4fb19ed98c700
RobinDort <robin@alexanderdort.com>
2025-02-11 08:41:47 +0100

    Added the german translation for the cookiebar

---
+++ src/Resources/contao/languages/de/default.php
@@ -0,0 +1,18 @@
+<?php
+
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_fh'] = '<h4 class="pslzme-heading">Sehr geehrte/r BesucherIn,';
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_sh'] = '<h4 class="pslzme-heading">Sie sind über <span id="pslzme-cookiebar-first-contact"></span> einem persönlichen pslz<b>me</b> Einladungslink von <span id="pslzme-cookiebar-link-creator"></span> gefolgt.</h4></p>
+<p>Hierdurch sind wir in der Lage, Sie auf unserer Webseite DSGVO-konform persönlich anzusprechen und Ihnen unsere Webseite mit persönlich für Sie zugeschnittenen Inhalten zu präsentieren, sofern Sie uns dies gestatten.</p>
+<p>Um dies zu tun, geben Sie bitte hier die ersten drei Buchstaben Ihres Nachnamens ein und bestätigen Sie dann zusätzlich mit einem Klick auf den <b>Ja-Button</b>, dass wir Sie persönlich auf unserer Webseite empfangen und Ihnen individuelle Inhalte und Angebote ausliefern dürfen. Oder lehnen Sie die Personalisierung mit Klick auf <strong>Nein</strong> ab. Sofern Sie Nein klicken, werden wir Sie auf die unpersonalisierte Version unserer Webseite weiterleiten.
+</p>';
+
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_footer_more_info'] = 'Weiterführende Informationen zu pslz<b>me</b>';
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_footer_text'] = '<p>Bei pslz<b>me</b> (ausgesprochen: personalize <b>me</b>) handelt es sich um ein von uns entwickeltes, DSGVO-konformes und absolut sicheres Personalisierungs-Framework, das schon jetzt als bahnbrechende Entwicklung im Programmatic Web bezeichnet werden darf und auf sichere und datensparsame Weise, erstmals Personalisierung, Individualisierung und persönliche Ansprache auch im Web ermöglicht.</p>
+<p>Unser System ist dabei so aufgebaut, dass alle Informationen unter Einsatz höchster Sicherheitsmaßnahmen auf unterschiedlichen Sicherheitsebenen in einen einfachen Link gepackt werden. So sicher, dass unser System selbst Ihre Daten nicht kennt und ebenso wir diese nicht tracken können. Es findet auch zu keinem Zeitpunkt eine unverschlüsselte Datenübertragung und -speicherung statt. Wir arbeiten hier ausschließlich mit personengebundenen Daten, die Sie freiwillig veröffentlicht haben. Es findet weder ein Tracking auf Basis Ihrer Daten, noch ein Profiling statt. Und pslz<b>me</b> entschlüsselt und verarbeitet Ihre personengebundenen Daten erst und ausschließlich dann, wenn Sie uns dies ausdrücklich erlauben.</p>';
+
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['imprint'] = 'Impressum';
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['privacy'] = 'Datenschutz';
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['yes'] = 'Ja';
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['no'] = 'Nein';
+
+?>
---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_fh'] = "Dear visitor,";
+
+?>
