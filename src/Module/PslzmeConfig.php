9127c2f2a76a3154d5e42ca5c0c92ca1fe6af0fd
RobinDort <robin@alexanderdort.com>
2025-02-11 12:29:11 +0100

    Removed some of the asset files and created a seperate module for the configuration

---
+++ src/Module/PslzmeConfig.php
@@ -0,0 +1,13 @@
+<?php
+namespace RobinDort\PslzmeLinks\Module;
+
+use Contao\Module;
+
+class PslzmeConfig extends Module {
+    protected $strTemplate = "mod_pslzme_config";
+
+    protected function compile() {}
+}
+
+
+?>
--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,19 +1,21 @@
 <?php
 use RobinDort\PslzmeLinks\Module\QueryDecryption;
 use RobinDort\PslzmeLinks\Module\PslzmeCookiebar;
+use RobinDort\PslzmeLinks\Module\PslzmeConfig;
+
 
 // Init all css / js files
 $GLOBALS['TL_CSS'][] = "bundles/robindortpslzmelinks/css/pslzme-cookiebar.css|static";
 
 $GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/url-query-data-filter.js|static";
 $GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/cookie-extractor.js|static";
-$GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/api-request.js|static";
+// $GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/api-request.js|static";
 $GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/redirect-cookie-acception.js|static";
 $GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/query-click-listener.js|static";
 $GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/pslzme-cookiebar-controller.js|static";
 $GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/pslzme-cookiebar-name-and-greeting-verifyer.js|static";
 $GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/cookie-acception.js|static";
-$GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/main.js|static";
+// $GLOBALS['TL_JAVASCRIPT'][] = "bundles/robindortpslzmelinks/js/main.js|static";
 
 
 // Set the CSRF token to be able to send ajax requests via js
@@ -21,5 +23,7 @@ $GLOBALS['TL_MOOTOOLS'][] = '<meta name="csrf-token" content="' . System::getCon
 
 $GLOBALS['FE_MOD']['pslzme']['query_decryption'] = QueryDecryption::class;
 $GLOBALS['FE_MOD']['pslzme']['pslzme_cookiebar'] = PslzmeCookiebar::class;
+$GLOBALS['FE_MOD']['pslzme']['pslzme_configuration'] = PslzmeConfig::class;
+
 
 ?>
