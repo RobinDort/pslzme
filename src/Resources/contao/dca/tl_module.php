5e237550b9688d96ea96a7db03834a34f92699e6
RobinDort <robin@alexanderdort.com>
2025-02-11 12:38:31 +0100

    Added palettes for the new module

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -5,5 +5,7 @@ use Contao\Controller;
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['query_decryption'] = '{title_legend},name,type;{config_legend},{template_legend},customTpl;{protected_legend:hide},protected;{expert_legend:hide}';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['pslzme_cookiebar'] = '{title_legend},name,type;{config_legend},{template_legend},customTpl;{protected_legend:hide},protected;{expert_legend:hide}';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['pslzme_configuration'] = '{title_legend},name,type;{config_legend},{template_legend},customTpl;{protected_legend:hide},protected;{expert_legend:hide}';
+
 
 ?>
