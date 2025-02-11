4c9512fb7b20aa17e71dd8163d092acac2938583
RobinDort <robin@alexanderdort.com>
2025-02-11 14:58:24 +0100

    uncommented the controller function again

--- src/Controller/RequestHandlerController.php
+++ src/Controller/RequestHandlerController.php
@@ -20,41 +20,41 @@ class RequestHandlerController {
     }
 
     public function __invoke(Request $request): JsonResponse {
-        // $postData = json_decode($request->getContent(), true);
+        $postData = json_decode($request->getContent(), true);
         
-        // if (!isset($postData['request']) || !isset($postData['data'])) {
-        //     return new JsonResponse(["error" => "post data[request] or postdata[data] not set!"], 400);
-        // }
+        if (!isset($postData['request']) || !isset($postData['data'])) {
+            return new JsonResponse(["error" => "post data[request] or postdata[data] not set!"], 400);
+        }
 
-        // $requestFunction = $postData['request'];
-        // $requestData = json_decode($postData['data'], true);
+        $requestFunction = $postData['request'];
+        $requestData = json_decode($postData['data'], true);
 
          $response = ["test"];
 
-        // switch ($requestFunction) {
-        //     case "query-acception":
-        //         $resp = $this->api->handleQueryAcception($requestData);
-        //         $response = $resp;
-        //     break;
+        switch ($requestFunction) {
+            case "query-acception":
+                $resp = $this->api->handleQueryAcception($requestData);
+                $response = $resp;
+            break;
 
-        //     case "query-lock-check":
-        //         $resp = $this->api->handleQueryLockCheck($requestData);
-        //         $response = $resp;
-        //     break;
+            case "query-lock-check":
+                $resp = $this->api->handleQueryLockCheck($requestData);
+                $response = $resp;
+            break;
 
-        //     case "extract-greeting-data":
-        //         $resp = $this->api->handleGreetingDataExtraction($requestData);
-        //         $response = $resp;
-        //         break;
+            case "extract-greeting-data":
+                $resp = $this->api->handleGreetingDataExtraction($requestData);
+                $response = $resp;
+                break;
 
-        //     case "compare-link-owner":
-        //         $resp = $this->api->handleCompareLinkOwner($requestData);
-        //         $response = $resp;
-        //         break;
+            case "compare-link-owner":
+                $resp = $this->api->handleCompareLinkOwner($requestData);
+                $response = $resp;
+                break;
 
-        //     default:
-        //         return new JsonResponse("Request does not match one of the provided availabilities");
-        // }
+            default:
+                return new JsonResponse("Request does not match one of the provided availabilities");
+        }
 
          return new JsonResponse($response);
     }
