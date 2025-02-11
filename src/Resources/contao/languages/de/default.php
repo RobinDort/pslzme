d4f219d201ac0273626aae70ac2daaeb1822ffc9
RobinDort <robin@alexanderdort.com>
2025-02-11 08:47:26 +0100

    Added the english translation for the cookiebar

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_fh'] = '<h4 class="pslzme-heading">Sehr geehrte/r BesucherIn,';
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_fh'] = '<h4 class="pslzme-heading">Sehr geehrte/r BesucherIn,</h4>';
 $GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_sh'] = '<h4 class="pslzme-heading">Sie sind über <span id="pslzme-cookiebar-first-contact"></span> einem persönlichen pslz<b>me</b> Einladungslink von <span id="pslzme-cookiebar-link-creator"></span> gefolgt.</h4></p>
 <p>Hierdurch sind wir in der Lage, Sie auf unserer Webseite DSGVO-konform persönlich anzusprechen und Ihnen unsere Webseite mit persönlich für Sie zugeschnittenen Inhalten zu präsentieren, sofern Sie uns dies gestatten.</p>
 <p>Um dies zu tun, geben Sie bitte hier die ersten drei Buchstaben Ihres Nachnamens ein und bestätigen Sie dann zusätzlich mit einem Klick auf den <b>Ja-Button</b>, dass wir Sie persönlich auf unserer Webseite empfangen und Ihnen individuelle Inhalte und Angebote ausliefern dürfen. Oder lehnen Sie die Personalisierung mit Klick auf <strong>Nein</strong> ab. Sofern Sie Nein klicken, werden wir Sie auf die unpersonalisierte Version unserer Webseite weiterleiten.
--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,5 +1,16 @@
 <?php
 
-$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_fh'] = "Dear visitor,";
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_fh'] = '<h4 class="pslzme-heading">Dear visitor,</h4>';
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_sh'] = '<h4 class="pslzme-heading">You have followed a personal pslz<b>me</b> invitation link from <span id="pslzme-cookiebar-link-creator"></span> via <span id="pslzme-cookiebar-first-contact"></span></h4></p>
+<p>This enables us to address you personally and GDPR-compliant on our website and to present our website to you with content tailored to you personally, provided you allow us to do so.</p>
+<p>To do this, please enter the first three letters of your surname here and then click on the <b>Yes</b> button to confirm that we may receive you personally on our website and deliver personalized content and offers to you. Or click <strong>No</strong> to reject personalization. If you click No, we will redirect you to the non-personalized version of our website.</p>';
 
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_footer_more_info'] = 'Further information on pslz<b>me</b>';
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['cookiebar_footer_text'] = '<p>pslz<b>me</b> (pronounced: personalize <b>me</b>) is a GDPR-compliant and absolutely secure personalization framework developed by us, which can already be described as a groundbreaking development in the programmatic web and enables personalization, individualization and a personal approach on the web for the first time in a secure and data-saving manner.</p>
+<p>Our system is structured in such a way that all information is packed into a simple link using the highest security measures at different security levels. So secure that our system itself does not know your data and we cannot track it either. At no time does unencrypted data transmission or storage take place. We also work exclusively with personal data that you have published voluntarily. There is neither tracking based on your data nor profiling. And pslz<b>me</b> only decrypts and processes your personal data if you expressly allow us to do so.</p>';
+
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['imprint'] = 'Imprint';
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['privacy'] = 'Privacy statement';
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['yes'] = 'Yes';
+$GLOBALS['TL_LANG']['robindort_pslzme_links']['no'] = 'No';
 ?>
