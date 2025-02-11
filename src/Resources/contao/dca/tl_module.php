3e324f996e5e3e0e6cc252321b3d0bb2a286c6b9
RobinDort <robin@alexanderdort.com>
2025-02-11 08:58:08 +0100

    Added the new modules palletes

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -4,6 +4,6 @@ use RobinDort\PslzmeLinks\Controller\FrontendModule\QueryDecryptionController;
 use Contao\Controller;
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['query_decryption'] = '{title_legend},name,type;{config_legend},{template_legend},customTpl;{protected_legend:hide},protected;{expert_legend:hide}';
-
+$GLOBALS['TL_DCA']['tl_module']['palettes']['pslzme_cookiebar'] = '{title_legend},name,type;{config_legend},{template_legend},customTpl;{protected_legend:hide},protected;{expert_legend:hide}';
 
 ?>
