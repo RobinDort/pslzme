091d9ebff819cabf1ec7dbfba5338f1ea7cc5870
RobinDort <robin@alexanderdort.com>
2025-02-11 08:55:52 +0100

    Added the module class for the cookiebar

---
+++ src/Module/PslzmeCookiebar.php
@@ -0,0 +1,13 @@
+<?php
+namespace RobinDort\PslzmeLinks\Module;
+
+use Contao\Module;
+
+class PslzmeCookiebar extends Module {
+    protected $strTemplate = "mod_pslzme_cookiebar";
+
+    protected function compile() {}
+}
+
+
+?>
