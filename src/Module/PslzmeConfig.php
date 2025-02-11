3d3527a24bd0198978cae4edbb56249d7b7b7225
RobinDort <robin@alexanderdort.com>
2025-02-11 14:08:45 +0100

    The config module now extends from FrontendModule of contao instead of the normal Module class

--- src/Module/PslzmeConfig.php
+++ src/Module/PslzmeConfig.php
@@ -1,12 +1,22 @@
 <?php
 namespace RobinDort\PslzmeLinks\Module;
 
-use Contao\Module;
+use Contao\FrontendModule;
+use Contao\FrontendTemplate;
+use Contao\Controller;
+use Contao\Environment;
 
-class PslzmeConfig extends Module {
+class PslzmeConfig extends FrontendModule {
     protected $strTemplate = "mod_pslzme_config";
 
     protected function compile() {}
+
+    public function generate() {
+        $this->Template = new FrontendTemplate($this->strTemplate);
+        $this->compile();
+
+        return $this->Template->parse();
+    }
 }
 
 
