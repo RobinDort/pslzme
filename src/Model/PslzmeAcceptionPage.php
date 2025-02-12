7d8c8051407fd417d617d44a7f295637d31c2d83
RobinDort <robin@alexanderdort.com>
2025-02-12 14:10:46 +0100

    Added new folder for model classes. Added new class to handle the pslzme acception page

---
+++ src/Model/PslzmeAcceptionPage.php
@@ -0,0 +1,152 @@
+<?php
+namespace RobinDort\PslzmeLinks\Model;
+
+use Contao\PageModel;
+
+class PslzmeAcceptionPage extends PageModel {
+
+    /**
+     * Properties extended from Contaos PageModel class.
+     * @property integer $id
+     * @property integer $pid
+     * @property integer $sorting
+     * @property integer $tstamp
+     * @property string  $title
+     * @property string  $alias
+     * @property string  $type
+     * @property string  $pageTitle
+     * @property string  $language
+     * @property string  $robots
+     * @property string  $description
+     * @property string  $redirect
+     * @property integer $jumpTo
+     * @property string  $url
+     * @property boolean $target
+     * @property string  $dns
+     * @property string  $staticFiles
+     * @property string  $staticPlugins
+     * @property boolean $fallback
+     * @property string  $adminEmail
+     * @property string  $dateFormat
+     * @property string  $timeFormat
+     * @property string  $datimFormat
+     * @property boolean $createSitemap
+     * @property string  $sitemapName
+     * @property boolean $useSSL
+     * @property boolean $autoforward
+     * @property boolean $protected
+     * @property string  $groups
+     * @property boolean $includeLayout
+     * @property integer $layout
+     * @property integer $mobileLayout
+     * @property boolean $includeCache
+     * @property integer $cache
+     * @property boolean $includeChmod
+     * @property integer $cuser
+     * @property integer $cgroup
+     * @property string  $chmod
+     * @property boolean $noSearch
+     * @property string  $cssClass
+     * @property string  $sitemap
+     * @property boolean $hide
+     * @property boolean $guests
+     * @property integer $tabindex
+     * @property boolean $accesskey
+     * @property boolean $published
+     * @property string  $start
+     * @property string  $stop
+     * @property array   $trail
+     * @property string  $mainAlias
+     * @property string  $mainTitle
+     * @property string  $mainPageTitle
+     * @property string  $parentAlias
+     * @property string  $parentTitle
+     * @property string  $parentPageTitle
+     * @property string  $folderUrl
+     * @property integer $rootId
+     * @property string  $rootAlias
+     * @property string  $rootTitle
+     * @property string  $rootPageTitle
+     * @property string  $domain
+     * @property string  $rootLanguage
+     * @property boolean $rootIsPublic
+     * @property boolean $rootIsFallback
+     * @property boolean $rootUseSSL
+     * @property string  $rootFallbackLanguage
+     * @property array   $subpages
+     * @property string  $outputFormat
+     * @property string  $outputVariant
+     * @property boolean $hasJQuery
+     * @property boolean $hasMooTools
+     * @property boolean $isMobile
+     * @property string  $template
+     * @property string  $templateGroup
+     * */
+
+
+    protected static $strTable = 'tl_page';
+
+    private const PAGE_TITLE = "Pslzme-Acception";
+    private const PAGE_TYPE = "regular";
+    private const PAGE_ROBOTS = "index,follow";
+    private const PAGE_REDIRECT = "permanent";
+    private const PAGE_SITEMAP = "map_default";
+
+
+    public function __construct() {
+        parent::__construct();
+
+        $unixTime = time();
+        
+        // Set the sorting of the new page directly after the parent id.
+        $this->sorting = $this->pid + 1; 
+        $this->tstamp = $unixTime;
+        $this->title = self::PAGE_TITLE;
+        $this->pageTitle = self::PAGE_TITLE;
+        $this->alias = strtolower(self::PAGE_TITLE);
+        $this->type = self::PAGE_TYPE;
+        $this->robots = self::PAGE_ROBOTS;
+        $this->redirect = self::PAGE_REDIRECT;
+        $this->jumpTo = 0;
+        $this->includeLayout = true;
+        $this->layout = 2;
+        $this->chmod = serialize(array(
+            0 => "u1",
+            1 => "u2",
+            2 => "u3",
+            3 => "u4",
+            4 => "u5",
+            5 => "u6",
+            6 => "g4",
+            7 => "g5",
+            8 => "g6"
+        ));
+        $this->sitemap = self::PAGE_SITEMAP;
+        $this->hide = false;
+        $this->published = true;
+    }
+
+    public function selfExists() {
+        // Check if the page is already existent.
+        $existentPage = PageModel::findByTitle(self::PAGE_TITLE);
+        if ($existentPage !== null) {
+           return true;
+        }
+  
+        return false;
+    }
+  
+    public function getTitle() {
+    return $this->title;
+    }
+  
+    public function getID() {
+    return $this->id;
+    }
+  
+    public function setParentPageID($parentPageID) {
+        $this->pid = $parentPageID;
+    }
+}
+
+?>
