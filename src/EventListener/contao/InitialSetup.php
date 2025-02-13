4cbe9757ecc5dbfd3a7a2b972e2710ed4be1ea7d
RobinDort <robin@alexanderdort.com>
2025-02-13 10:38:21 +0100

    Instead of using a local variable now the contao config is used

--- src/EventListener/contao/InitialSetup.php
+++ src/EventListener/contao/InitialSetup.php
@@ -1,6 +1,7 @@
 <?php
 namespace RobinDort\PslzmeLinks\EventListener\contao;
 
+use Contao\Config;
 use RobinDort\PslzmeLinks\Model\PslzmeAcceptionPage;
 use RobinDort\PslzmeLinks\Model\PslzmeDeclinePage;
 use RobinDort\PslzmeLinks\Model\PslzmeAcceptionArticle;
@@ -8,49 +9,50 @@ use RobinDort\PslzmeLinks\Model\PslzmeAcceptionArticle;
 class InitialSetup {
 
     public function runSetup() {
-        $initDone = false;
-
-        if ($initDone === false) {
-            // Create new pages for pslzme redirection (accepted, locked)
-            $pslzmeAcceptionPage = new PslzmeAcceptionPage();
-            $pslzmeDeclinePage = new PslzmeDeclinePage();
-
-            // Check if the page already exists.
-            if (!$pslzmeAcceptionPage->selfExists()) {
-
-                // Get parent page ID.
-                $pid = $pslzmeAcceptionPage->selectActivePageRootID();
-
-                // Set the parent page ID to the new page.
-                $pslzmeAcceptionPage->setParentPageID($pid);
-
-                // Save the new page.
-                $pslzmeAcceptionPage->save();
-
-                // create the new pslzme acception article
-                $pslzmeAcceptionArticle = new PslzmeAcceptionArticle();
-
-                // save the new article when not existent
-                if (!$pslzmeAcceptionArticle->selfExists()) {
-                    $pslzmeAcceptionArticle->save();
-                }
-            }
-
-            // Do the same check for the other page
-            if (!$pslzmeDeclinePage->selfExists()) {
-
-                // Get parent page ID.
-                $pid = $pslzmeDeclinePage->selectActivePageRootID();
-
-                // Set the parent page ID to the new page.
-                $pslzmeDeclinePage->setParentPageID($pid);
-
-                // Save the new page.
-                $pslzmeDeclinePage->save();
-            }
-
-            $initDone = true;
+        if (Config::get('pslzme_setup_done')) {
+            return; // Exit if setup was already completed
         }
+
+        // Create new pages for pslzme redirection (accepted, locked)
+        $pslzmeAcceptionPage = new PslzmeAcceptionPage();
+        $pslzmeDeclinePage = new PslzmeDeclinePage();
+
+        // Check if the page already exists.
+        if (!$pslzmeAcceptionPage->selfExists()) {
+
+            // Get parent page ID.
+            $pid = $pslzmeAcceptionPage->selectActivePageRootID();
+
+            // Set the parent page ID to the new page.
+            $pslzmeAcceptionPage->setParentPageID($pid);
+
+            // Save the new page.
+            $pslzmeAcceptionPage->save();
+
+            // create the new pslzme acception article
+            $pslzmeAcceptionArticle = new PslzmeAcceptionArticle();
+
+            // save the new article when not existent
+            if (!$pslzmeAcceptionArticle->selfExists()) {
+                $pslzmeAcceptionArticle->save();
+            }
+        }
+
+        // Do the same check for the other page
+        if (!$pslzmeDeclinePage->selfExists()) {
+
+            // Get parent page ID.
+            $pid = $pslzmeDeclinePage->selectActivePageRootID();
+
+            // Set the parent page ID to the new page.
+            $pslzmeDeclinePage->setParentPageID($pid);
+
+            // Save the new page.
+            $pslzmeDeclinePage->save();
+        }
+
+        // Mark setup as done
+        Config::set('pslzme_setup_done', true);
     }
 }
 
