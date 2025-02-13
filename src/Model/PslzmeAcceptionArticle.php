8d95a5c5733c5ddd2d874639126229ebcc63a379
RobinDort <robin@alexanderdort.com>
2025-02-13 14:48:48 +0100

    Added the inColumn attribute to the AcceptionArticle

--- src/Model/PslzmeAcceptionArticle.php
+++ src/Model/PslzmeAcceptionArticle.php
@@ -50,6 +50,7 @@ class PslzmeAcceptionArticle extends ArticleModel implements CustomArticleModel{
         $this->author = $this->findAuthorID();
         $this->sorting = 128;
         $this->tstamp = $time;
+        $this->inColumn = "main";
         $this->published = true;
     }
 
