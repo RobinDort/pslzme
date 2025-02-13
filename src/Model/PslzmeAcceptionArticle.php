7caac7678bd87717f4c4733ac9d6e5cdbd62010c
RobinDort <robin@alexanderdort.com>
2025-02-13 08:20:12 +0100

    Instead of using findbyTitle from the PageModel now findOneByTitle is used to only return a PageModel when found

--- src/Model/PslzmeAcceptionArticle.php
+++ src/Model/PslzmeAcceptionArticle.php
@@ -61,12 +61,12 @@ class PslzmeAcceptionArticle extends ArticleModel {
 
     public function findParentPageID() {
         // find the parent page and its ID by searching for its title. Same title as the articles.
-        $parentPage = PageModel::findByTitle(self::ARTICLE_TITLE);
+        $parentPage = PageModel::findOneByPageTitle(self::ARTICLE_TITLE);
 
         // Check if $parentPage is a collection or a single model, then return its ID
-        if ($parentPage instanceof \Model\Collection) {
-            return $parentPage->current()->id ?? 1;
-        }
+        // if ($parentPage instanceof \Model\Collection) {
+        //     return $parentPage->current()->id ?? 1;
+        // }
         return $parentPage->id ?? 1;
     }
 }
