3dc4bbf515e2145cc9471fb1283f3a5b0c23433d
RobinDort <robin@alexanderdort.com>
2025-02-14 10:05:36 +0100

    Added the dca for the new element

---
+++ src/Resources/contao/dca/tl_content.php
@@ -0,0 +1,32 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['pslzme_text'] = '{type_legend},type,headline;{text_legend},personalizedText,unpersonalizedText;{expert_legend:hide},cssID';
+
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['personalizedTextGroup'] = [
+    'label' => array('Personalized Text/HTML/Code'),
+    'inputType' => 'group',
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['personalizedText'] = [
+    'label'     => ['Personalized Text', 'Enter personalized text'],
+    'exclude'   => true,
+    'inputType' => 'textarea',
+    'eval'      => ['rte' => 'tinyMCE', 'helpwizard' => true, 'mandatory' => false],
+    'sql'       => ['type' => 'text', 'notnull' => false],
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['personalizedTextGroup'] = [
+    'label' => array('Unpersonalized Text/HTML/Code'),
+    'inputType' => 'group',
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['unpersonalizedText'] = [
+    'label'     => ['Unpersonalized Text', 'Enter unpersonalized text'],
+    'exclude'   => true,
+    'inputType' => 'textarea',
+    'eval'      => ['rte' => 'tinyMCE', 'helpwizard' => true, 'mandatory' => true],
+    'sql'       => ['type' => 'text', 'notnull' => false],
+];
+
+?>
