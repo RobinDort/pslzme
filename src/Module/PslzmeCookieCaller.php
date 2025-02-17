4fcb954d7859917dcfa078afa587a1e091f20736
RobinDort <robin@alexanderdort.com>
2025-02-17 10:40:57 +0100

    Added a new module class for the pslzme cookie caller

---
+++ src/Module/PslzmeCookieCaller.php
@@ -0,0 +1,10 @@
+<?php
+namespace RobinDort\PslzmeLinks\Module;
+
+class PslzmeCookieCaller extends Module {
+    protected $strTemplate = "mod_pslzme_cookie_caller";
+
+    protected function compile() {}
+}
+
+?>
