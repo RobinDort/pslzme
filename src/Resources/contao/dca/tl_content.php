a58fc8f8249b8aa50eaf2eaa6b317d43aa20d877
RobinDort <robin@alexanderdort.com>
2025-02-17 14:54:50 +0100

    Adjusted the order of the palette elements

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -39,20 +39,13 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['unpersonalizedText'] = [
  * Configuration for pslzme_3D_content element
  */
 
- $GLOBALS['TL_DCA']['tl_content']['palettes']['pslzme_3D_content'] = '
- {type_legend},type,headline;
- {text_legend},pageLink,html,addImage;
- {personalized_legend},personalizedLinkGroup;
- {3d_legend},3dPlayGround;
- {image_legend},singleSRC,alt,size,imagemargin,imageUrl,fullsize,caption,floating;
- {expert_legend:hide},cssID
+$GLOBALS['TL_DCA']['tl_content']['palettes']['pslzme_3D_content'] = '
+    {type_legend},type,headline;
+    {personalized_legend},personalizedLinkGroup,addImage;
+    {3d_legend},3dPlayGround,pageLink,html;
+    {image_legend},singleSRC,alt,size,imagemargin,imageUrl,fullsize,caption,floating;
+    {expert_legend:hide},cssID
 ';
-
-$GLOBALS['TL_DCA']['tl_content']['fields']['personalizedLinkGroup'] = [
-    'label' => array('Personalized image'),
-    'inputType' => 'group',
-    'eval' => ['tl_class' => 'clr'], // Ensures it appears on a new line
-];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['3dPlayGround'] = [
     'label' => array('3D image'),
