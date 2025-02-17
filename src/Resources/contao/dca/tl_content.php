f904f8f0961e53d665090b8d9bb264b451c4548f
RobinDort <robin@alexanderdort.com>
2025-02-17 14:09:08 +0100

    Added the configuration (fields and palettes) for the new element

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Configuration for pslzme_text element
+ */
+
 $GLOBALS['TL_DCA']['tl_content']['palettes']['pslzme_text'] = '{type_legend},type,headline;{text_legend},personalizedText,unpersonalizedText;{expert_legend:hide},cssID';
 
 
@@ -27,6 +31,37 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['unpersonalizedText'] = [
     'inputType' => 'textarea',
     'eval'      => ['rte' => 'tinyMCE', 'helpwizard' => true, 'mandatory' => true],
     'sql' => "TEXT NULL"
+];
+
+
+
+/**
+ * Configuration for pslzme_3D_content element
+ */
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['pslzme_3D_content'] = '{type_legend},type,headline;{text_legend},pageLink,html,addImage,arrData,cssID';
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['personalizedLinkGroup'] = [
+    'label' => array('Personalized image'),
+    'inputType' => 'group',
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['addImage'] = [
+    'inputType' => 'standardField',
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['3dPlayGround'] = [
+    'label' => array('3D image'),
+    'inputType' => 'group',
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['pageLink'] = [
+    'label' => array('Link address'),
+    'inputType' => 'pageTree',
+];
+
+$GLOBALS['TL_DCA']['tl_content']['fields']['html'] = [
+    'inputType' => 'standardField',
 ];
 
 ?>
