6a5c413417158ecd03e6bff79e54c39fb6e14ce0
RobinDort <robin@alexanderdort.com>
2025-02-17 12:08:41 +0100

    Updated the pslzme-cookie-caller-controller js file and added more function to be able to display and hide the caller as well as display the cookiebar when the caller is clicked on

--- src/Resources/public/js/pslzme-cookie-caller-controller.js
+++ src/Resources/public/js/pslzme-cookie-caller-controller.js
@@ -1,4 +1,16 @@
-$(document).ready(function () {
+const addVisibleClassesToElement = (element) => {
+	element.style.visibility = "visible";
+
+	element.classList.remove("animation", "slideOutRight", "slow");
+	element.classList.add("animation", "slideInRight", "slow");
+};
+
+const addHiddenClassesToElement = (element) => {
+	element.classList.remove("animation", "slideInRight", "slow");
+	element.classList.add("animation", "slideOutRight", "slow");
+};
+
+const addCookieCallerClickListener = () => {
 	$("#pslzme-cookie-caller").click(function (event) {
 		$("#pslzme-cookiebar").css({
 			display: "flex",
@@ -6,4 +18,27 @@ $(document).ready(function () {
 			"justify-content": "center",
 		});
 	});
-});
+};
+
+const controlCookieCaller = () => {
+	let html = document.querySelector("html");
+	const pslzmeCookieCaller = document.getElementById("pslzme-cookie-caller");
+
+	const queriesAreSet = queryParamsSet();
+
+	// Add listener so the cookiebar can be displayed when clicking on the caller.
+	addCookieCallerClickListener();
+
+	// Add another listener to scroll behavior to display the caller when the user browses the page.
+	window.addEventListener("scroll", function (event) {
+		if (html.scrollTop >= 110) {
+			if (queriesAreSet.isSet === true) {
+				addVisibleClassesToElement(pslzmeCookieCaller);
+			}
+		} else if (html.scrollTop <= 110) {
+			if (queriesAreSet.isSet === true) {
+				addHiddenClassesToElement(pslzmeCookieCaller);
+			}
+		}
+	});
+};
