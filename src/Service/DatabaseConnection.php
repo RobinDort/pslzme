9dc6602e3c8556361a634b14994e0440c2ca6097
RobinDort <robin@alexanderdort.com>
2025-02-18 11:25:04 +0100

    Adjusted the DatabaseConnection service. Now it will be checked first if the pslzme database already exists. If not it will be created specifically. If it does exists a connection will be established like before

--- src/Service/DatabaseConnection.php
+++ src/Service/DatabaseConnection.php
@@ -18,13 +18,22 @@ class DatabaseConnection {
 
 
         try {
-            // create connection
-            $this->connection = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+            // first check if the pslzme database exists
+            $dbExists = databaseExists();
 
-            // check if connection was established
-            if($this->connection->connect_error) {
-                throw new \Exception("Connection failed: " . $this->connection->connect_error);
-            } 
+            if (!$dbExists) {
+                // The database does not exist and must therefore be created first
+                createPslzmeDatabase();
+            } else {
+                // The database exists and the connection can be established
+                // create connection
+                $this->connection = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+
+                // check if connection was established
+                if($this->connection->connect_error) {
+                    throw new \Exception("Connection to database failed: " . $this->connection->connect_error);
+                } 
+            }   
         } catch (Exception $e) {
             error_log($e->getMessage());
             // Rethrow the exception so the caller knows the connection failed
@@ -40,6 +49,56 @@ class DatabaseConnection {
         if ($this->connection !== null) {
             $this->connection->close();
         }
+    }
+
+    private function databaseExists() {
+
+        // establish a connection to the mysql server. NOT to the database in order to check for the database existence
+        $conn = new mysqli($this->servername, $this->username, $this->password);
+
+        // check if connection was established
+        if($conn->connect_error) {
+            throw new \Exception("Connection to mysql server failed: " . $conn->connect_error);
+        } 
+
+        // connection established
+        // select the database by its name
+        $sqlQuery = "SHOW DATABASES LIKE '" . $this->dbname . "'";
+        $result = $conn->query($sqlQuery);
+
+        // Close the connection after the query
+        $conn->close();
+
+        if ($result->num_rows === 0) {
+            // Database does not exist
+            return false;
+        } else {
+            // Database already exists
+            return true;
+        }
+    }
+
+
+    private function createPslzmeDatabase() {
+        // establish a connection to the mysql server. NOT to the database in order to create it
+        $conn = new mysqli($this->servername, $this->username, $this->password);
+
+        // check if connection was established
+        if($conn->connect_error) {
+            throw new \Exception("Connection to mysql server failed: " . $conn->connect_error);
+        }
+
+        $sqlQuery = "CREATE DATABASE " . $this->dbname;
+        $result = $conn->query($sqlQuery);
+
+        $conn->close();
+        
+        if ($result) {
+            \System::Log("Database created successfully.", __METHOD__,TL_GENERAL);
+        } else {
+            throw new Exception("Error while trying to create pslzme_customer database");
+        }
+
     }
 }
 ?>
