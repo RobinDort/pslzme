8c6e8d400f9d0624d8ca4d3315fe8bcde6a51061
RobinDort <robin@alexanderdort.com>
2025-02-26 15:23:13 +0100

    Using encryption for the database password

--- src/Controller/BackendRequestHandlerController.php
+++ src/Controller/BackendRequestHandlerController.php
@@ -5,6 +5,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Attribute\AsController;
+use Contao\Database;
+use Contao\Encryption;
+
+use RobinDort\PslzmeLinks\Exceptions\InvalidDataException;
 
 #[AsController]
 class BackendRequestHandlerController {
@@ -12,7 +16,24 @@ class BackendRequestHandlerController {
     #[Route('/saveDatabaseData', name: "save_database_data")]
     public function saveDatabaseData(Request $request): JsonResponse {
         $requestData = $request->request->get('data');
-        return new JsonResponse($requestData);
+        try {
+            $databaseName = $requestData->dbName;
+            $databaseUser = $requestData->dbUsername;
+            $databasePassword = $requestData->dbPW;
+
+            if (!$databaseName || !$databaseUser || !$databasePassword) {
+                throw new InvalidDataException("Unable to extract database information out of request object");
+            }
+
+            // encrypt the password before saving
+            $encryptedPassword = Encryption::encrypt($databasePassword);
+
+            // save the database data into the pslzme config table
+        } catch (InvalidDataException $ide) {
+            error_log($ide->getErrorMsg());
+        }
+
+        return new JsonResponse($encryptedPassword);
     }
 
 }
