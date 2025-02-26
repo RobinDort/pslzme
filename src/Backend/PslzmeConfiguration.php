d01c689ec0952b0def7ada8c6d38807f0a6849c4
RobinDort <robin@alexanderdort.com>
2025-02-26 09:32:50 +0100

    Added a new backend class that acts as the controller for the backend module

---
+++ src/Backend/PslzmeConfiguration.php
@@ -0,0 +1,19 @@
+<?php
+namespace RobinDort\PslzmeLinks\Backend;
+
+use Contao\BackendModule;
+
+class PslzmeConfiguration extends BackendModule {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function compile() {}
+
+    public function generate() {}
+}
+?>
