40f0678b6588153976d64547c73c977fd2a3ff65
RobinDort <robin@alexanderdort.com>
2025-02-27 12:41:17 +0100

    made decrypt function private and returned the extracted values

--- src/Controller/BackendRequestHandlerController.php
+++ src/Controller/BackendRequestHandlerController.php
@@ -22,43 +22,45 @@ class BackendRequestHandlerController {
             throw new InvalidDataException("Unable to extract request data out of /saveDatabaseData object");
         }
 
-        // try {
-        //     $databaseName = $requestData->dbName;
-        //     $databaseUser = $requestData->dbUsername;
-        //     $databasePassword = $requestData->dbPW;
+        try {
+            $databaseName = $requestData->dbName;
+            $databaseUser = $requestData->dbUsername;
+            $databasePassword = $requestData->dbPW;
 
-        //     if (!$databaseName || !$databaseUser || !$databasePassword) {
-        //         throw new InvalidDataException("Unable to extract database information out of request object");
-        //     }
+            $testArr = [$databaseName, $databaseUser, $databasePassword];
+            return new JsonResponse($testArr);
 
-        //     // encrypt the password before saving
-        //     $timestamp = time();
-        //     $encryptedPassword = $this->encryptPassword($databasePassword,$timestamp);
+            // if (!$databaseName || !$databaseUser || !$databasePassword) {
+            //     throw new InvalidDataException("Unable to extract database information out of request object");
+            // }
 
-        //     // save the database data into the pslzme config table
-        //     $db = Database::getInstance();
-        //     if (!$db) {
-        //         throw new DatabaseException("Unable to connect to contao database");
-        //     }
+            // // encrypt the password before saving
+            // $timestamp = time();
+            // $encryptedPassword = $this->encryptPassword($databasePassword,$timestamp);
 
-        //     $result = $db->prepare("INSERT INTO tl_pslzme_config (pslzme_db_name, pslzme_db_user, pslzme_db_pw, timestamp) VALUES (?,?,?,?)")->execute($databaseName, $databaseUser, $encryptedPassword, $timestamp);
+            // // save the database data into the pslzme config table
+            // $db = Database::getInstance();
+            // if (!$db) {
+            //     throw new DatabaseException("Unable to connect to contao database");
+            // }
 
-        //     if ($result->affectedRows > 0) {
-        //         return new JsonResponse("Sucessfully inserted pslzme database data.");
-        //     } else {
-        //         throw new DatabaseException("Unable to insert pslzme configuration data into tl_pslzme_config table");
-        //     }
-        // } catch (InvalidDataException $ide) {
-        //     error_log($ide->getErrorMsg());
-        //     return new JsonResponse($ide->getErrorMsg());
-        // } catch(DatabaseException $dbe) {
-        //     error_log($dbe->getErrorMsg());
-        //     return new JsonResponse($dbe->getErrorMsg());
-        // } catch (Exception $e) {
-        //     error_log($e->getMessage());
-        //     return new JsonResponse($e->getMessage());
-        // }
-        return new JsonResponse($requestData);
+            // $result = $db->prepare("INSERT INTO tl_pslzme_config (pslzme_db_name, pslzme_db_user, pslzme_db_pw, timestamp) VALUES (?,?,?,?)")->execute($databaseName, $databaseUser, $encryptedPassword, $timestamp);
+
+            // if ($result->affectedRows > 0) {
+            //     return new JsonResponse("Sucessfully inserted pslzme database data.");
+            // } else {
+            //     throw new DatabaseException("Unable to insert pslzme configuration data into tl_pslzme_config table");
+            // }
+        } catch (InvalidDataException $ide) {
+            error_log($ide->getErrorMsg());
+            return new JsonResponse($ide->getErrorMsg());
+        } catch(DatabaseException $dbe) {
+            error_log($dbe->getErrorMsg());
+            return new JsonResponse($dbe->getErrorMsg());
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            return new JsonResponse($e->getMessage());
+        }
     }
 
 
@@ -72,7 +74,7 @@ class BackendRequestHandlerController {
         return $encryptedData;
     }  
 
-    function decryptPassword($encryptedPassword, $timestamp) {
+    private function decryptPassword($encryptedPassword, $timestamp) {
         $secretKey = hash('sha256', $timestamp); // Recreate key from timestamp
         $data = base64_decode($encryptedPassword);
         
