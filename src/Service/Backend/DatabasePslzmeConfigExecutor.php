0fa36156627709b9070c0d3c0bb87b6b0807a9b4
RobinDort <robin@alexanderdort.com>
2025-02-27 15:29:21 +0100

    Added a new class DatabasePslzmeConfigExecutor that will handle all the database function needed inside the newly created backend module

---
+++ src/Service/Backend/DatabasePslzmeConfigExecutor.php
@@ -0,0 +1,13 @@
+<?php
+namespace RobinDort\PslzmeLinks\Service\Backend;
+
+use Contao\Database;
+
+class DatabasePslzmeConfigExecutor {
+    public function __construct() {}
+
+    public function insertDatabaseConfigurationData($databaseName, $databaseUser, $databasePW) {
+
+    }
+}
+?>
