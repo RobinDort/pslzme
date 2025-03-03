0c50dfe0fcab0b616057f70e539f8b0ba5be39b5
RobinDort <robin@alexanderdort.com>
2025-03-03 08:55:47 +0100

    Fixed typo in variable access

--- src/Service/Backend/DatabasePslzmeConfigStmtPreparer.php
+++ src/Service/Backend/DatabasePslzmeConfigStmtPreparer.php
@@ -11,13 +11,13 @@ class DatabasePslzmeConfigStmtPreparer {
 
     public function prepareSelectPslzmeDBConfig() {
         $sqlQuery = "SELECT * FROM tl_pslzme_config";
-        $stmt = $this->connection->prepare($stmt);
+        $stmt = $this->connection->prepare($sqlQuery);
         return $stmt;
     }
 
     public function prepareUpdatePslzmeDBConfig($databaseName, $databaseUser, $databasePW) {
         $sqlQuery = "UPDATE tl_pslzme_config SET pslzme_db_name = ?, pslzme_db_user = ?, pslzme_db_pw = ?";
-        $stmt = $this->connection->prepare($stmt);
+        $stmt = $this->connection->prepare($sqlQuery);
         $stmt->bind_param("sss", $databaseName, $databaseUser, $databasePW);
 
         return $stmt;
@@ -26,7 +26,7 @@ class DatabasePslzmeConfigStmtPreparer {
 
     public function prepareInsertPslzmeDBConfig($databaseName, $databaseUser, $databasePW, $timestamp) {
         $sqlQuery = "INSERT INTO tl_pslzme_config (pslzme_db_name, pslzme_db_user, pslzme_db_pw, timestamp) VALUES (?,?,?,?)";
-        $stmt = $this->connection->prepare($stmt);
+        $stmt = $this->connection->prepare($sqlQuery);
         $stmt->bind_param("sssi", $databaseName, $databaseUser, $databasePW, $timestamp);
 
         return $stmt;
