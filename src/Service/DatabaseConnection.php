975380d75d82c7b41aa478cac0b7f114682f4977
RobinDort <robin@alexanderdort.com>
2025-03-07 09:05:50 +0100

    injected the DatabasePslzmeConfigStmtExecutor class

--- src/Service/DatabaseConnection.php
+++ src/Service/DatabaseConnection.php
@@ -14,6 +14,7 @@ use Exception;
 
 class DatabaseConnection {
     private $connection;
+    private $dbStmtExecutor;
     // private $servername;
     // private $username;
     // private $password;
@@ -43,10 +44,11 @@ class DatabaseConnection {
     // }
 
 
-    public function __construct() {
+    public function __construct(DatabasePslzmeConfigStmtExecutor $dbPslzmeCSE) {
         try {
             // Get the database data
-            $dbStmtExecutor = new DatabasePslzmeConfigStmtExecutor();
+            //$dbStmtExecutor = new DatabasePslzmeConfigStmtExecutor();
+            $this->dbStmtExecutor = $dbPslzmeCSE;
             $dbData = $dbStmtExecutor->selectCurrentDatabaseConfigurationData();
 
             if(empty($dbData["databaseUser"]) || empty($dbData["databasePassword"]) || empty($dbData["databaseTimestamp"]) || empty($dbData["databaseName"])) {
