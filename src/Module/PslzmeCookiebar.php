03f8c0e74879a53f57f1ecbd50945503ad03bef1
RobinDort <robin@alexanderdort.com>
2025-03-12 13:53:17 +0100

    Adjusted function to inject DatabasePslzmeConfigStmtExecutor

--- src/Module/PslzmeCookiebar.php
+++ src/Module/PslzmeCookiebar.php
@@ -4,32 +4,40 @@ namespace RobinDort\PslzmeLinks\Module;
 use RobinDort\PslzmeLinks\Service\Backend\DatabasePslzmeConfigStmtExecutor;
 
 use Contao\Module;
+use Contao\ModuleModel;
 
 class PslzmeCookiebar extends Module {
     protected $strTemplate = "mod_pslzme_cookiebar";
 
-    private $dbStmtExecutor;
+    private ?DatabasePslzmeConfigStmtExecutor $dbStmtExecutor = null;
     private $imprintID;
     private $privacyID;
 
-    public function setDbStmtExecutor(DatabasePslzmeConfigStmtExecutor $dbStmtExecutor){
+
+    public function __construct(ModuleModel $objModule) {
+        parent::__construct($objModule);
+    }
+
+
+    public function setDbStmtExecutor(DatabasePslzmeConfigStmtExecutor $dbStmtExecutor) {
+        $this->dbStmtExecutor = $dbStmtExecutor;
+
         try {
-            $this->dbStmtExecutor = $dbStmtExecutor;
             $dbConfigData = $this->dbStmtExecutor->selectCurrentDatabaseConfigurationData();
             $internalPageRefs = $dbConfigData["databaseIPR"];
 
             if (!empty($internalPageRefs)) {
-                $internalPageRefs = json_decode($internalPageRefs,true);
-                $this->imprintId = $internalPageRefs["Imprint"];
-                $this->privacyID = $internalPageRefs["Privacy"];
+                $internalPageRefs = json_decode($internalPageRefs, true);
+                $this->imprintID = $internalPageRefs["Imprint"] ?? null;
+                $this->privacyID = $internalPageRefs["Privacy"] ?? null;
             }
-
         } catch (DatabaseException $dbe) {
             error_log($dbe->getErrorMsg());
         } catch (Exception $e) {
             error_log($e->getMessage());
         }
     }
+
 
     protected function compile() {}
 
