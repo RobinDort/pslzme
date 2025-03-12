760f34cc885194153ea3cd3496c86bd83c3edc28
RobinDort <robin@alexanderdort.com>
2025-03-12 08:38:30 +0100

    Added new functions to the CustomPageModel interface

--- src/Interfaces/CustomPageModel.php
+++ src/Interfaces/CustomPageModel.php
@@ -6,7 +6,9 @@ interface CustomPageModel {
     public function selfExists();
     public function selectActivePageRootID();
     public function findMostUsedLayoutID();
+    public function findLatestSorting($pid);
     public function setParentPageID($parentPageID);
+    public function setSorting($sorting);
     public function getTitle();
     public function getID();
 }
