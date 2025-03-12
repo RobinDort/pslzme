c37c640ecb96518340e7723018bc7c29829e2f37
RobinDort <robin@alexanderdort.com>
2025-03-12 12:38:52 +0100

    Added the generate function to the PslzmeCookiebar module

--- src/Module/PslzmeCookiebar.php
+++ src/Module/PslzmeCookiebar.php
@@ -9,10 +9,21 @@ class PslzmeCookiebar extends Module {
     protected $strTemplate = "mod_pslzme_cookiebar";
 
     private $dbStmtExecutor;
+    private $imprintID;
+    private $privacyID;
 
     public function __construct(DatabasePslzmeConfigStmtExecutor $dbStmtExecutor) {
         try {
             $this->dbStmtExecutor = $dbStmtExecutor;
+            $dbConfigData = $this->dbStmtExecutor->selectCurrentDatabaseConfigurationData();
+            $internalPageRefs = $dbConfigData["databaseIPR"];
+
+            if (!empty($internalPageRefs)) {
+                $internalPageRefs = json_decode($internalPageRefs,true);
+                $this->imprintId = $internalPageRefs["Imprint"];
+                $this->privacyID = $internalPageRefs["Privacy"];
+            }
+
         } catch (DatabaseException $dbe) {
             error_log($dbe->getErrorMsg());
         } catch (Exception $e) {
@@ -21,6 +32,15 @@ class PslzmeCookiebar extends Module {
     }
 
     protected function compile() {}
+
+
+    public function generate() {
+        $this->Template->imprintID = $this->imprintID;
+        $this->Template->privacyID = $this->privacyID;
+        $this->compile();
+
+        return $this->Template->parse();
+    }
 }
 
 
