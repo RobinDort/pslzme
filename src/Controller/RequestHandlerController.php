05d8793df5332190cf32edf4d6eb7fffa8b40f9f
RobinDort <robin@alexanderdort.com>
2025-03-27 15:24:55 +0100

    Added documentation to the RequestHandlerController

--- src/Controller/RequestHandlerController.php
+++ src/Controller/RequestHandlerController.php
@@ -8,21 +8,28 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use RobinDort\PslzmeLinks\Service\Api;
 
-
+/**
+ * Class that handles all frontend requests from this extension
+ */
 #[Route('/requestHandler', name: RequestHandlerController::class)] 
 #[AsController]
 class RequestHandlerController {
 
     private $api;
 
+    /**
+     * constructor
+     */
     public function __construct(Api $api) {
         $this->api = $api;
     }
 
-    // public function __construct() {
-    //     $this->api = new Api();
-    // }
-
+    /**
+     * function that gets called when a request to the /requestHandler route is sent.
+     * depending on the requestFunction object a different api function will be called.
+     * @request: request object containing requestData and requestFunction to handle different requests by a single route
+     * @returns a JsonResponse object containing information about the functionality used decided by the requestFunction object.
+     */
     public function __invoke(Request $request): JsonResponse {
         $requestData = $request->request->get('data');
         $requestFunction = $request->request->get('request');
