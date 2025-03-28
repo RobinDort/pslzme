f0c5b05cfc45e37d5822c7c927fb50974cebbc41
RobinDort <robin@alexanderdort.com>
2025-03-28 13:12:30 +0100

    Added the first pslzme tag replacement

--- src/EventListener/contao/ReplaceInsertTags.php
+++ src/EventListener/contao/ReplaceInsertTags.php
@@ -1,10 +1,42 @@
 <?php
 namespace RobinDort\PslzmeLinks\EventListener\contao;
 
+use RobinDort\PslzmeLinks\Exceptions\InvalidFileException;
+
+use Exception;
+
 class ReplaceInsertTags {
     public function replacePslzmeInsertTags($tag) {
-        \System::log("Tag content: " . $tag, __METHOD__,"TL_ERROR");
-        throw new \Exception("Tag Debug: " . $tag);
+        // Check if the tag starts with "pslzme-"
+        if (!str_starts_with($tag, 'pslzme:')) {
+            return;
+        }
+
+        // Everything after 'pslzme:'
+        $usedTemplateTag = substr($tag,7);
+        $fileContent;
+
+        try {
+            switch ($usedTemplateTag) {
+                case "firstname":
+                    $templateFile = "print-firstname.html5";
+                    $templatePath =  System::getContainer()->getParameter('kernel.project_dir') . "/templates/outputs/$templateFile";
+
+                    // If the file exists, return its content
+                    if (file_exists($templatePath)) {
+                        $fileContent = file_get_contents($templatePath);
+                    } else {
+                        throw new InvalidFileException("File with path: " . $fileContent . " does not exist");
+                    }
+                break;
+            }
+        } catch (InvalidFileException $ife) {
+            error_log($ife->getErrorMsg());
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
+
+        return $fileContent;
     }
 }
 
