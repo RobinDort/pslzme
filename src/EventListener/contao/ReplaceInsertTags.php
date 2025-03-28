85c5a9641247dde1ac3d2b5536874e4d4e57dbe5
RobinDort <robin@alexanderdort.com>
2025-03-28 13:29:16 +0100

    Added missing file extension to the filePath

--- src/EventListener/contao/ReplaceInsertTags.php
+++ src/EventListener/contao/ReplaceInsertTags.php
@@ -17,27 +17,25 @@ class ReplaceInsertTags {
         $usedTemplateTag = substr($tag,7);
         $fileContent;
 
-        throw new Exception("Template Tag: " . $usedTemplateTag);
+        try {
+            switch ($usedTemplateTag) {
+                case "firstname":
+                    $templateFile = "print-firstname.html5";
+                    $templatePath =  System::getContainer()->getParameter('kernel.project_dir') . "/templates/outputs/$templateFile.html5";
 
-        // try {
-        //     switch ($usedTemplateTag) {
-        //         case "firstname":
-        //             $templateFile = "print-firstname.html5";
-        //             $templatePath =  System::getContainer()->getParameter('kernel.project_dir') . "/templates/outputs/$templateFile";
-
-        //             // If the file exists, return its content
-        //             if (file_exists($templatePath)) {
-        //                 $fileContent = file_get_contents($templatePath);
-        //             } else {
-        //                 throw new InvalidFileException("File with path: " . $fileContent . " does not exist");
-        //             }
-        //         break;
-        //     }
-        // } catch (InvalidFileException $ife) {
-        //     error_log($ife->getErrorMsg());
-        // } catch (Exception $e) {
-        //     error_log($e->getMessage());
-        // }
+                    // If the file exists, return its content
+                    if (file_exists($templatePath)) {
+                        $fileContent = file_get_contents($templatePath);
+                    } else {
+                        throw new InvalidFileException("File with path: " . $fileContent . " does not exist");
+                    }
+                break;
+            }
+        } catch (InvalidFileException $ife) {
+            error_log($ife->getErrorMsg());
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
 
         return $fileContent;
     }
