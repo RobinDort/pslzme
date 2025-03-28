8c66ffe07b65e6a13596af8d656388822566d584
RobinDort <robin@alexanderdort.com>
2025-03-28 12:42:02 +0100

    Adjusted error msg

--- src/EventListener/contao/ReplaceInsertTags.php
+++ src/EventListener/contao/ReplaceInsertTags.php
@@ -4,7 +4,7 @@ namespace RobinDort\PslzmeLinks\EventListener\contao;
 class ReplaceInsertTags {
     public function replacePslzmeInsertTags($tag) {
         \System::log("Tag content: " . $tag, __METHOD__,"TL_ERROR");
-        throw new \Exception("Tag Debug");
+        throw new \Exception("Tag Debug: " . $tag);
     }
 }
 
