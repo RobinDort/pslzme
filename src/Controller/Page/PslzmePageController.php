66ea2929210452021fbde6be3694be2596cf00a5
RobinDort <robin@alexanderdort.com>
2025-04-09 15:48:15 +0200

    Instead of returning hello world now the controller returns the assigned template to it

--- src/Controller/Page/PslzmePageController.php
+++ src/Controller/Page/PslzmePageController.php
@@ -6,7 +6,9 @@ use Symfony\Component\HttpFoundation\Response;
 class PslzmePageController {
     public function __invoke(): Response
     {
-        return new Response('Hello World!');
+        // Initialize the template
+        $template = new Template('fe_page_pslzme');
+        return new Response($template->parse());
     }
 }
 ?>
