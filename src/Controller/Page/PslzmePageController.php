39b425ebdf9e598615e4c208b1a76822b3bdae5d
RobinDort <robin@alexanderdort.com>
2025-04-09 15:40:17 +0200

    Registered the new page controller inside the services yaml file

--- src/Controller/Page/PslzmePageController.php
+++ src/Controller/Page/PslzmePageController.php
@@ -1,10 +1,8 @@
 <?php
 namespace RobinDort\PslzmeLinks\Controller\Page;
 
-use Contao\CoreBundle\DependencyInjection\Attribute\AsPage;
 use Symfony\Component\HttpFoundation\Response;
 
-#[AsPage]
 class PslzmePageController {
     public function __invoke(): Response
     {
