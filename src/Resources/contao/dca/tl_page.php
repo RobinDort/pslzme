4e395a7c61b5048ef4f96d573370c158a9bb52dd
RobinDort <robin@alexanderdort.com>
2025-04-09 15:03:27 +0200

    Extendes the palette for the new pslzme page from the regular page

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -1,3 +1,5 @@
 <?php
     $GLOBALS['TL_DCA']['tl_page']['fields']['type']['options'][] = 'pslzme';
+
+    $GLOBALS['TL_DCA']['tl_page']['palettes']['pslzme_page'] = $GLOBALS['TL_DCA']['tl_page']['palettes']['regular'];
 ?>
