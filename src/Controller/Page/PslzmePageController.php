7f1d07be2fb9d8ed9285c9a4b299559444182d5b
RobinDort <robin@alexanderdort.com>
2025-04-09 15:53:18 +0200

    instead of using template now the imported FrontendTemplate from contao is used

--- src/Controller/Page/PslzmePageController.php
+++ src/Controller/Page/PslzmePageController.php
@@ -2,12 +2,13 @@
 namespace RobinDort\PslzmeLinks\Controller\Page;
 
 use Symfony\Component\HttpFoundation\Response;
+use Contao\FrontendTemplate;
 
 class PslzmePageController {
     public function __invoke(): Response
     {
         // Initialize the template
-        $template = new Template('fe_page_pslzme');
+        $template = new FrontendTemplate('fe_page_pslzme');
         return new Response($template->parse());
     }
 }
