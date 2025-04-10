7782d1846173715d919ddf4e91be68149f503250
RobinDort <robin@alexanderdort.com>
2025-04-10 11:20:33 +0200

    Added a new class PslzmeNavigation that will handle a custom navbar that does not show pslzme pages. Also added the corresponding frontend template

---
+++ src/Module/PslzmeNavigation.php
@@ -0,0 +1,18 @@
+<?php
+namespace RobinDort\PslzmeLinks\Module;
+
+use Contao\ModuleNavigation;
+use Contao\PageModel;
+
+class PslzmeNavigation extends ModuleNavigation {
+
+    protected $strTemplate = "mod_pslzme_navbar";
+
+    protected function compile(): void {
+        parent::compile();
+        $this->Template->items = $this->Template->items;
+    }
+}
+
+
+?>
