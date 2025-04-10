c7a8d78ed34abe486d8d0a5b316027a92515b075
RobinDort <robin@alexanderdort.com>
2025-04-10 10:29:56 +0200

    removed the abstract page controller

--- src/Controller/Page/PslzmePageController.php
+++ src/Controller/Page/PslzmePageController.php
@@ -1,12 +1,11 @@
 <?php
 namespace RobinDort\PslzmeLinks\Controller\Page;
 
-use Contao\CoreBundle\Controller\AbstractPageController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Contao\FrontendTemplate;
 
-class PslzmePageController extends AbstractPageController
+class PslzmePageController
 {
     public function __invoke(Request $request): Response
     {
