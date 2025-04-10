95477fb86324b27f4fbdafff84f958e49f470edd
RobinDort <robin@alexanderdort.com>
2025-04-10 15:45:38 +0200

    Adjusted condition

--- src/Controller/Page/PslzmePageController.php
+++ src/Controller/Page/PslzmePageController.php
@@ -12,18 +12,16 @@ class PslzmePageController
 {
     public function __invoke(Request $request, PageModel $pageModel): Response
     {
-        $template = new FrontendTemplate('fe_page_pslzme');
+        if (empty($GLOBALS['decryptedVars']) || $GLOBALS['decryptedVars']['varsSet'] === false) {
+            $homepage = PageModel::findFirstPublishedRootByHostAndLanguage($request->getHost(), $pageModel->language);
 
-        // if (!$GLOBALS['decryptedVars'] || $GLOBALS['decryptedVars']['varsSet'] === false) {
-        //     $homepage = PageModel::findFirstPublishedRootByHostAndLanguage($request->getHost(), $pageModel->language);
+            if ($homepage !== null) {
+                return new RedirectResponse($homepage->getFrontendUrl());
+            }
 
-        //     if ($homepage !== null) {
-        //         return new RedirectResponse($homepage->getFrontendUrl());
-        //     }
-
-        //      // Fallback: redirect to root slash if homepage not found
-        //      return new RedirectResponse('/');
-        // }
+             // Fallback: redirect to root slash if homepage not found
+             return new RedirectResponse('/');
+        }
         return (new FrontendIndex())->renderPage($pageModel);
     }
 }
