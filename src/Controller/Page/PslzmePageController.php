1d498b320c10ae9ba37a63df3e3b1be156122705
RobinDort <robin@alexanderdort.com>
2025-04-10 15:22:27 +0200

    debug exception

--- src/Controller/Page/PslzmePageController.php
+++ src/Controller/Page/PslzmePageController.php
@@ -14,6 +14,8 @@ class PslzmePageController
     {
         $template = new FrontendTemplate('fe_page_pslzme');
 
+        throw new Exception("GLobal: " . print_r($GLOBALS));
+
         if (!$GLOBALS['decryptedVars'] || $GLOBALS['decryptedVars']['varsSet'] === false) {
             $homepage = PageModel::findFirstPublishedRootByHostAndLanguage($request->getHost(), $pageModel->language);
 
