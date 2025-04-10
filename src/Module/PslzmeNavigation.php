3c6ea25220e1ef145cd99031cff261f4bb89ed3a
RobinDort <robin@alexanderdort.com>
2025-04-10 14:11:55 +0200

    Removed the generate again because of an error

--- src/Module/PslzmeNavigation.php
+++ src/Module/PslzmeNavigation.php
@@ -9,13 +9,6 @@ class PslzmeNavigation extends ModuleNavigation {
     protected function compile(): void {
         parent::compile();
     }
-
-    public function generate() {
-        $this->navigationTpl = 'nav_pslzme';
-        $this->compile();
-
-        return $this->Template->parse();
-    }
 }
 
 
