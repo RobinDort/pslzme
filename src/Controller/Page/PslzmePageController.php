804ceee4e02b825220402323699ee0367d7bff15
RobinDort <robin@alexanderdort.com>
2025-04-10 10:47:10 +0200

    using the frontend index to render the page

--- src/Controller/Page/PslzmePageController.php
+++ src/Controller/Page/PslzmePageController.php
@@ -3,14 +3,16 @@ namespace RobinDort\PslzmeLinks\Controller\Page;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Contao\PageModel;
 use Contao\FrontendTemplate;
+use Contao\FrontendIndex;
 
 class PslzmePageController
 {
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, PageModel $pageModel): Response
     {
         $template = new FrontendTemplate('fe_page_pslzme');
-        return new Response($template->parse());
+        return (new FrontendIndex())->renderPage($pageModel);
     }
 }
 
