b8bdf910bd75d28c5017fe25f013e5d64bef220c
RobinDort <robin@alexanderdort.com>
2025-04-10 15:10:03 +0200

    Added custom logic so when pslzme variables are not set then the pslzme page type will redirect to the homepage

--- src/Controller/Page/PslzmePageController.php
+++ src/Controller/Page/PslzmePageController.php
@@ -3,6 +3,7 @@ namespace RobinDort\PslzmeLinks\Controller\Page;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Contao\PageModel;
 use Contao\FrontendTemplate;
 use Contao\FrontendIndex;
@@ -12,6 +13,17 @@ class PslzmePageController
     public function __invoke(Request $request, PageModel $pageModel): Response
     {
         $template = new FrontendTemplate('fe_page_pslzme');
+
+        if ($GLOBALS['decryptedVars']['varsSet'] === false) {
+            $homepage = PageModel::findFirstPublishedRootByHostAndLanguage($request->getHost(), $pageModel->language);
+
+            if ($homepage !== null) {
+                return new RedirectResponse($homepage->getFrontendUrl());
+            }
+
+             // Fallback: redirect to root slash if homepage not found
+             return new RedirectResponse('/');
+        }
         return (new FrontendIndex())->renderPage($pageModel);
     }
 }
