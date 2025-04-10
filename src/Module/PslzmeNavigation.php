b2a96946d74a48cc9344ed46b889a4dcab28498d
RobinDort <robin@alexanderdort.com>
2025-04-10 12:20:48 +0200

    debug exception

--- src/Module/PslzmeNavigation.php
+++ src/Module/PslzmeNavigation.php
@@ -10,7 +10,7 @@ class PslzmeNavigation extends ModuleNavigation {
 
     protected function compile(): void {
         parent::compile();
-        $this->Template->items = $this->Template->items;
+        throw new \Exception("Items: " . var_dump($this->items));
     }
 }
 
