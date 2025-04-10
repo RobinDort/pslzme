94c0b5f689036cc1275f2c29c08448162093b34b
RobinDort <robin@alexanderdort.com>
2025-04-10 08:01:00 +0200

    Adjusted the getPageLayout hook function

--- src/EventListener/PageLayoutListener.php
+++ src/EventListener/PageLayoutListener.php
@@ -3,11 +3,12 @@ namespace RobinDort\PslzmeLinks\EventListener;
 
 use Contao\PageModel;
 use Contao\LayoutModel;
+use Contao\PageRegular;
 
 class PageLayoutListener {
-    public function __invoke(PageModel $pageModel, LayoutModel $layoutModel): void
+    public function __invoke(PageModel $pageModel, LayoutModel $layoutModel, PageRegular $pageRegular): void
     {
-        if ($pageModel->type === 'pslzme_page') {
+        if ($pageModel->type === 'pslzme') {
             $layoutModel->template = 'fe_page_pslzme';
         }
     }
