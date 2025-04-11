182d843c09dc1ee7f742ff67a2271ea314542a8c
RobinDort <robin@alexanderdort.com>
2025-04-11 08:15:01 +0200

    Also removed the not needed custom template mod_navigation_pslzme for the navbar

--- src/Module/PslzmeNavigation.php
+++ src/Module/PslzmeNavigation.php
@@ -6,8 +6,6 @@ use Contao\PageModel;
 
 class PslzmeNavigation extends ModuleNavigation {
 
-    protected $strTemplate = 'mod_navigation_pslzme';
-
     protected function compile(): void {
         parent::compile();
     }
