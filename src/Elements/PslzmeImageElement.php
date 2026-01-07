644fba04126271c1c93f0a1d3a115f0fada4f9ea
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-01-07 10:44:47 +0100

    Added the new class PslzmeImageElement that controls the new element

---
+++ src/Elements/PslzmeImageElement.php
@@ -0,0 +1,12 @@
+<?php
+namespace RobinDort\PslzmeLinks\Elements;
+
+use Contao\ContentElement;
+
+class PslzmeImageElement extends ContentElement {
+    protected $strTemplate = 'ce_pslzme_image';
+
+    protected function compile() {
+       
+    }
+}
