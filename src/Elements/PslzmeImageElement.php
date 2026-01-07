5c1ac06935cc1cb43cd1cae233d159fb29d02fff
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-01-07 11:13:22 +0100

    Added the Template variables inside the compile function

--- src/Elements/PslzmeImageElement.php
+++ src/Elements/PslzmeImageElement.php
@@ -7,6 +7,11 @@ class PslzmeImageElement extends ContentElement {
     protected $strTemplate = 'ce_pslzme_image';
 
     protected function compile() {
-       
+        $this->Template->firstImage = $this->firstImage;
+        $this->Template->firstImageSize = $this->firstImageSize;
+        $this->Template->secondImage = $this->secondImage;
+        $this->Template->secondImageSize = $this->secondImageSize;
+        $this->Template->personalizedText = $this->personalizedText;
+        $this->Template->unpersonalizedText = $this->unpersonalizedText;
     }
 }
