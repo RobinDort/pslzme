a98c76aaee9a1d87683f575e45e4a3aac3b669e7
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-01-09 13:58:40 +0100

    changed all the binding parameters inside StatementPreparer to use the new doctrine syntax

--- src/Service/StatementPreparer.php
+++ src/Service/StatementPreparer.php
@@ -20,7 +20,8 @@ class StatementPreparer {
     public function prepareSelectCustomerKey($queryParamID) {
         $sqlQuery = "SELECT a.*, b.* FROM pslzme_kunde as a INNER JOIN encrypt_info AS b ON a.KundenID=b.PslzmeKundenID WHERE a.KundenID=?"; 
         $stmt = $this->conn->prepare($sqlQuery);
-        $stmt->bind_param("i", $queryParamID);
+
+        $stmt->bindValue(1,$queryParamID,"integer");
 
         return $stmt;
     }
@@ -28,7 +29,10 @@ class StatementPreparer {
     public function prepareSelectCustomerQuery($queryParamTimestamp, $queryParamCustomerID, $queryParamEncryptionID) {
         $sqlQuery = "SELECT * FROM query_link WHERE CreationTime=? AND PslzmeKundenID=? AND EncryptInfoID=?"; 
         $stmt = $this->conn->prepare($sqlQuery);
-        $stmt->bind_param("iii", $queryParamTimestamp, $queryParamCustomerID, $queryParamEncryptionID);
+
+        $stmt->bindValue(1,$queryParamTimestamp,"integer");
+        $stmt->bindValue(2,$queryParamCustomerID,"integer");
+        $stmt->bindValue(3,$queryParamEncryptionID,"integer");
 
         return $stmt;
     }
@@ -39,7 +43,13 @@ class StatementPreparer {
     public function prepareUpdateCustomerQuery($queryParamTimestamp, $queryParamChangedOn, $queryParamCookieAccepted, $queryParamCustomerID, $queryParamEncryptionID, $queryParamQueryLocked) {
         $sqlQuery = "UPDATE query_link SET Accepted=?, Locked=?, ChangedOn=? WHERE CreationTime=? AND PslzmeKundenID=? AND EncryptInfoID=?"; 
         $stmt = $this->conn->prepare($sqlQuery);
-        $stmt->bind_param("iiiiii", $queryParamCookieAccepted, $queryParamQueryLocked, $queryParamChangedOn, $queryParamTimestamp, $queryParamCustomerID, $queryParamEncryptionID);
+
+        $stmt->bindValue(1,$queryParamCookieAccepted,"integer");
+        $stmt->bindValue(2,$queryParamQueryLocked,"integer");
+        $stmt->bindValue(3,$queryParamChangedOn,"integer");
+        $stmt->bindValue(4,$queryParamTimestamp,"integer");
+        $stmt->bindValue(5,$queryParamCustomerID,"integer");
+        $stmt->bindValue(6,$queryParamEncryptionID,"integer");
 
         return $stmt;
     }
@@ -50,7 +60,7 @@ class StatementPreparer {
     public function prepareInsertCustomer($queryParamCustomer) {
         $sqlQuery = "INSERT INTO pslzme_kunde (Name) VALUES (?)";
         $stmt = $this->conn->prepare($sqlQuery);
-        $stmt->bind_param("s", $queryParamCustomer);
+        $stmt->bindValue(1,$queryParamCustomer,"string");
 
         return $stmt;
     }
@@ -58,7 +68,8 @@ class StatementPreparer {
     public function prepareInsertCustomerKey($queryParamEncryptionKey, $queryParamCustomerID) {
         $sqlQuery = "INSERT INTO encrypt_info (EncryptionKey, PslzmeKundenID) VALUES (?,?)";
         $stmt = $this->conn->prepare($sqlQuery);
-        $stmt->bind_param("ss", $queryParamEncryptionKey, $queryParamCustomerID);
+        $stmt->bindValue(1,$queryParamEncryptionKey,"string");
+        $stmt->bindValue(2,$queryParamCustomerID,"string");
 
         return $stmt;
     }
@@ -66,7 +77,14 @@ class StatementPreparer {
     public function prepareInsertCustomerQuery($queryParamQuery, $queryParamTimestamp, $queryParamAcceptedOn, $queryParamCookieAccepted, $queryParamCustomerID, $queryParamEncryptionID, $queryParamQueryLocked) {
         $sqlQuery = "INSERT INTO query_link (QueryString, CreationTime, AcceptionTime, Accepted, Locked, PslzmeKundenID, EncryptInfoID) VALUES(?,?,?,?,?,?,?)"; 
         $stmt = $this->conn->prepare($sqlQuery);
-        $stmt->bind_param("siiiiii", $queryParamQuery, $queryParamTimestamp, $queryParamAcceptedOn, $queryParamCookieAccepted, $queryParamQueryLocked, $queryParamCustomerID, $queryParamEncryptionID);
+
+        $stmt->bindValue(1,$queryParamQuery,"string");
+        $stmt->bindValue(2,$queryParamTimestamp,"integer");
+        $stmt->bindValue(3,$queryParamAcceptedOn,"integer");
+        $stmt->bindValue(4,$queryParamCookieAccepted,"integer");
+        $stmt->bindValue(5,$queryParamQueryLocked,"integer");
+        $stmt->bindValue(6,$queryParamCustomerID,"integer");
+        $stmt->bindValue(7,$queryParamEncryptionID,"integer");
 
         return $stmt;
     }
