755669d73c699a754e05efa80124fb9b8e293285
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-01-13 10:15:31 +0100

    Added a new module class for contao 5 that handles the query decryption

---
+++ src/Controller/FrontendModule/QueryDecryptionController.php
@@ -0,0 +1,16 @@
+<?php
+namespace RobinDort\PslzmeLinks\Controller\FrontendModule;
+
+use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+use Contao\CoreBundle\Twig\FragmentTemplate;
+use Contao\ModuleModel;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class QueryDecryptionController extends AbstractFrontendModuleController {
+    protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response {
+        return $template->getResponse();
+    }
+}
+?>
