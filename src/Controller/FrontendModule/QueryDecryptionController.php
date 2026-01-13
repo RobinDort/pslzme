47ac9358e76015132a3d9de47b582d607f8204c8
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-01-13 10:38:14 +0100

    injected the database connection to the query decryption Controller class to pass it to the template

--- src/Controller/FrontendModule/QueryDecryptionController.php
+++ src/Controller/FrontendModule/QueryDecryptionController.php
@@ -7,9 +7,19 @@ use Contao\ModuleModel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use RobinDort\PslzmeLinks\Service\DatabaseConnection;
+
 
 class QueryDecryptionController extends AbstractFrontendModuleController {
+
+    private $conn;
+
+    public function __construct(DatabaseConnection $conn) {
+        $this->conn = $conn;
+    }
     protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response {
+
+        $template->connection = $this->conn;
         return $template->getResponse();
     }
 }
