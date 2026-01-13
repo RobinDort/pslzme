bf7c1b72835d383b1e3363aeba608f6f0585c6b5
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-01-13 15:04:31 +0100

    Add PslzmeCookieCaller frontend module controller as module for contao 5

---
+++ src/Controller/FrontendModule/PslzmeCookieCallerController.php
@@ -0,0 +1,17 @@
+<?php
+namespace RobinDort\PslzmeLinks\Controller\FrontendModule;
+
+use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+use Contao\CoreBundle\Twig\FragmentTemplate;
+use Contao\ModuleModel;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PslzmeCookieCaller extends AbstractFrontendModuleController {
+
+     protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response {
+        return $template->getResponse();
+    }
+}
+
+?>
