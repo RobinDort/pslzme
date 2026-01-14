387e5134f8836c9743bec055a8e7c5cf70a6e607
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-01-14 15:20:13 +0100

    Added the new dca attributes to the content elements class to use them inside the template

--- src/Elements/PslzmeImageElement.php
+++ src/Elements/PslzmeImageElement.php
@@ -7,10 +7,20 @@ class PslzmeImageElement extends ContentElement {
     protected $strTemplate = 'ce_pslzme_image';
 
     protected function compile() {
+        $this->Template->contenSpaceUnit = $this->contentSpaceUnit;
+        $this->Template->contentSpaceTop = $this->contentSpaceTop;
+        $this->Template->contentSpaceRight = $this->contentSpaceRight;
+        $this->Template->contentSpaceBottom = $this->contentSpaceBottom;
+        $this->Template->contentSpaceLeft = $this->contentSpaceLeft;
         $this->Template->firstImage = $this->firstImage;
         $this->Template->firstImageSize = $this->firstImageSize;
+        $this->Template->firstImageAlt = $this->firstImageAlt;
+        $this->Template->firstImageTitle = $this->firstImageTitle;
         $this->Template->secondImage = $this->secondImage;
         $this->Template->secondImageSize = $this->secondImageSize;
+        $this->Template->secondImageLink = $this->secondImageLink;
+        $this->Template->secondImageAlt = $this->secondImageAlt;
+        $this->Template->secondImageTitle = $this->secondImageTitle;
         $this->Template->personalizedText = $this->personalizedText;
         $this->Template->unpersonalizedText = $this->unpersonalizedText;
     }
