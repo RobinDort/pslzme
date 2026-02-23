fa38214b589ba51282160dec3194be5ca4cf02bd
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-02-23 10:36:24 +0100

    Fixed typos inside the language translation files

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -94,7 +94,7 @@ $GLOBALS['TL_LANG']['pslzme_configuration']['pslzme_explanation_part_four'] = 'S
                         Zur Erstellung des Users, navigieren sie zum Abschnitt <span>&lt;Datenbank-Nutzer erstellen&gt;</span>
                         und weissen Sie dann die gewünschten Konfigurationdaten wie Username und Passwort zu. Nach beidiger Erstellung
                         muss zuletzt - wenn nicht bereits getan - der erstellte User noch der Datenbank zugewiesen werden.';
-$GLOBALS['TL_LANG']['pslzme_configuration']['config_step_two'] = '<span>Schritt 2:</span> Datenbank an plsz<strong>me</strong> plugin anbinden';
+$GLOBALS['TL_LANG']['pslzme_configuration']['config_step_two'] = '<span>Schritt 2:</span> Datenbank an pslz<strong>me</strong> plugin anbinden';
 $GLOBALS['TL_LANG']['pslzme_configuration']['pslzme_explanation_part_five'] = 'Als nächstes tragen Sie bitte die Verbindungsdaten der soeben erstellten Datenbank in die nachstehenden Felder ein und bestätigen dies durch Klick des Speichern buttons.';
 $GLOBALS['TL_LANG']['pslzme_configuration']['db_name'] = "Datenbankname:";
 $GLOBALS['TL_LANG']['pslzme_configuration']['current_db'] = "Aktuelle Datenbank:";
