b51283b4001b7624eff7bc46e1c316f68f2f87b8
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-03-19 15:57:54 +0100

    Added seperate default values for each margin side

--- src/Elements/PslzmeImageElement.php
+++ src/Elements/PslzmeImageElement.php
@@ -9,22 +9,32 @@ class PslzmeImageElement extends ContentElement {
     protected function compile() {
         $unit = $this->contentSpaceUnit ?: 'px';
 
-        $contentSpaceValue = unserialize($this->contentSpace);
+        $contentSpaceValue = unserialize($this->contentSpace) ?: [];
+        $cpTop    = $contentSpaceValue['top'] ?? 0;
+        $cpRight  = $contentSpaceValue['right'] ?? 0;
+        $cpBottom = $contentSpaceValue['bottom'] ?? 0;
+        $cpLeft   = $contentSpaceValue['left'] ?? 0;
+        $cpUnit   = $contentSpaceValue['unit'] ?? 'px';
         $contentSpace = sprintf(
             "%s%s %s%s %s%s %s%s",
-            $contentSpaceValue['top'] ?? 0, $contentSpaceValue['unit'] ?? 'px',
-            $contentSpaceValue['right'] ?? 0, $contentSpaceValue['unit'] ?? 'px',
-            $contentSpaceValue['bottom'] ?? 0, $contentSpaceValue['unit'] ?? 'px',
-            $contentSpaceValue['left'] ?? 0, $contentSpaceValue['unit'] ?? 'px',
+            $cpTop, $cpUnit,
+            $cpRight, $cpUnit,
+            $cpBottom, $cpUnit,
+            $cpLeft, $cpUnit,
         );
 
-        $textSpaceValue = unserialize($this->textSpace);
+        $textSpaceValue = unserialize($this->textSpace) ?: [];
+        $tpTop    = $textSpaceValue['top'] ?? 0;
+        $tpRight  = $textSpaceValue['right'] ?? 0;
+        $tpBottom = $textSpaceValue['bottom'] ?? 0;
+        $tpLeft   = $textSpaceValue['left'] ?? 0;
+        $tpUnit   = $textSpaceValue['unit'] ?? 'px';
         $textSpace = sprintf(
             "%s%s %s%s %s%s %s%s",
-            $textSpaceValue['top'] ?? 0, $textSpaceValue['unit'] ?? 'px',
-            $textSpaceValue['right'] ?? 0, $textSpaceValue['unit'] ?? 'px',
-            $textSpaceValue['bottom'] ?? 0, $textSpaceValue['unit'] ?? 'px',
-            $textSpaceValue['left'] ?? 0, $textSpaceValue['unit'] ?? 'px',
+            $tpTop, $tpUnit,
+            $tpRight, $tpUnit,
+            $tpBottom, $tpUnit,
+            $tpLeft, $tpUnit,
         );
 
         $this->Template->contentSpace = $contentSpace;
