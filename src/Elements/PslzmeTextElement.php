9eae87548c6a82e5e2991f011c36e4b805b44eeb
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-03-27 08:37:47 +0100

    Added a should render condition inside the controller class to prevent contao from outputting content when nothing should be displayed

--- src/Elements/PslzmeTextElement.php
+++ src/Elements/PslzmeTextElement.php
@@ -11,6 +11,22 @@ class PslzmeTextElement extends ContentElement {
     protected $strTemplate = 'ce_pslzme_text';
 
     protected function compile() {
+        $shouldRender = false;
+
+        if ($varsSet) {
+            if (!empty($this->personalizedText) || ($this->showUnpersonalizedText && !empty($this->unpersonalizedText))) {
+                $shouldRender = true;
+            }
+        } else {
+            if ($this->showUnpersonalizedText && !empty($this->unpersonalizedText)) {
+                $shouldRender = true;
+            }
+        }
+
+        if (!$shouldRender) {
+            return;
+        }
+        
         $this->Template->personalizedText = $this->personalizedText;
         $this->Template->unpersonalizedText = $this->unpersonalizedText;
     }
